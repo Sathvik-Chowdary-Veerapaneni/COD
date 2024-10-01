0be5d666cbfd643c6fbbf7fca6b45a4bd5f078a3
Sathvik-Chowdary-Veerapaneni <sathvikchowdaryveerapaneni@gmail.com>
2024-09-30 22:50:54 -0500

    Added Title and Basic Input Text Box

--- web_application/main_page.py
+++ web_application/main_page.py
@@ -1,1 +1,8 @@
 import streamlit as st
+
+# Title of the app
+st.title('Country of Ideas')
+
+# Adding text input box
+user_input = st.text_input("Enter your idea:")
+st.write("You entered:", user_input)
